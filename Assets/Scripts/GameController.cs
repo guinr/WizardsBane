db784d76374bb61ab770115db027d753d926ba35
guinr <guigonunes@gmail.com>
2021-05-11 18:12:02 -0300

    Room generation improvement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Utils;
 using Random = UnityEngine.Random;
 
@@ -8,11 +7,9 @@ public class GameController : MonoBehaviour
 
     public GameObject rooms;
     public GameObject player;
-
     public string seed;
-    private const int Height = 5;
-    private const int Width = 5;
-    private string[,] _rooms;
+    private const int LevelCols = 5;
+    private const int LevelRows = 3;
 
     private void Start()
     {
@@ -21,83 +18,60 @@ public class GameController : MonoBehaviour
         var md5 = MD5Util.Md5Sum(seed);
 
         var chars = md5.ToCharArray();
-
-        var normalRooms = int.TryParse(chars[0].ToString(), out _);
         
-        _rooms = new string[Height, Width];
+        GenerateKingRoom();
+        GenerateBossRoom();
+        GenerateRandomRooms();
+    }
 
-        GenerateKingAndBossRooms(normalRooms);
-        GenerateSubBossesRooms();
-        GenerateRandomRooms(chars);
+    private void GenerateKingRoom()
+    {
+        GenerateRoom("king_room", 0, 0);
+        player.transform.position = new Vector3(10, -8.5f, 0);
+        player.SetActive(true);
+    }
 
-        var lastHeight = 0f;
-        var lastWidth = 0f;
+    private void GenerateBossRoom()
+    {
+        GenerateRoom("boss_room", LevelCols, LevelRows);
+    }
 
-        for (var i = 0; i < Height; i++)
+    private void GenerateRandomRooms()
+    {
+        for (var actualLevel = 0; actualLevel <= LevelRows; actualLevel++)
         {
-            for (var j = 0; j < Width; j++)
+            for (var actualCol = 0; actualCol <= LevelCols; actualCol++)
             {
-                var room = rooms.transform.Find(_rooms[i, j]);
+                if (actualCol == 0 && actualLevel == 0 || actualCol == LevelCols && actualLevel == LevelRows) continue;
 
-                var newRoom = Instantiate(room);
-                
-                newRoom.gameObject.SetActive(true);
-                var box = newRoom.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>();
-                var boxSize = box.size;
-                
-                newRoom.transform.position = new Vector3(lastHeight, lastWidth, 0);
-
-                lastHeight += boxSize.x;
-
-                if (j == Width -1)
+                if (actualLevel == 0 && actualCol == LevelCols ||
+                    actualLevel == 1 && actualCol == 0 ||
+                    actualLevel == 2 && actualCol == LevelCols ||
+                    actualLevel == 3 && actualCol == 0)
                 {
-                    lastHeight = 0f;
-                    lastWidth -= boxSize.y;                    
+                    GenerateMiniBossRoom(actualCol, actualLevel);
                 }
-
-                if (_rooms[i, j] != "king_room") continue;
-                
-                player.transform.position = new Vector3(boxSize.x * j + (normalRooms ? -boxSize.y * 1.3f : boxSize.y/3), -14 * i, 0);
-                player.SetActive(true);
+                else
+                {
+                    GenerateRoom("room_0", actualCol, actualLevel);
+                }
             }
         }
     }
 
-    private void GenerateKingAndBossRooms(bool normalRooms)
+    private void GenerateMiniBossRoom(int col, int row)
     {
-        if (normalRooms)
-        {
-            _rooms[0, 0] = "king_room";
-            _rooms[Height - 1, Width - 1] = "boss_room";
-        }
-        else
-        {
-            _rooms[0, Width - 1] = "king_room";
-            _rooms[Height - 1, 0] = "boss_room";
-        }
+        var miniBossRoomName = "mini_boss_room_" + row;
+        
+        GenerateRoom(miniBossRoomName, col, row);
     }
 
-    private void GenerateSubBossesRooms()
+    private void GenerateRoom(string roomName, int cols, int row)
     {
-        _rooms[1, Random.Range(0, Width - 1)] = "first_boss_room";
-        _rooms[2, Random.Range(0, Width - 1)] = "second_boss_room";
-        _rooms[3, Random.Range(0, Width - 1)] = "third_boss_room";
-    }
-
-    private void GenerateRandomRooms(char[] md5Shards)
-    {
-        var shardIndex = 0;
-
-        for (var i = 0; i < Height; i++)
-        {
-            for (var j = 0; j < Width; j++)
-            {
-                if (_rooms[i, j] != null) continue;
-
-                _rooms[i, j] = "room_" + md5Shards[shardIndex];
-                    
-                shardIndex++;
-            }
-        }
+        var room = rooms.transform.Find(roomName);
+        var newRoom = Instantiate(room);
+        newRoom.gameObject.SetActive(true);
+        
+        newRoom.transform.position = new Vector3(75 * cols, -75 * row, 0);
     }
 }
